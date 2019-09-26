422595d50b30b933e8eed317efd2e00d14817b3c
Thiago Cassimiro <43118264+woolz@users.noreply.github.com>
2019-09-26 15:19:53 -0300

    controle de estoque

---
+++ controle.php
@@ -0,0 +1,42 @@
+<?php
+require 'vendor/autoload.php';
+$app = new VeterinariaUnimonte\App;
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php
+    require 'includes/global.php';
+    ?>
+    <title><?php echo $app->name; ?> - Controle Estoque</title>
+  </head>
+  <body>
+    <!-- Navegação -->
+    <?php
+    $page_id = 6;
+    require 'includes/header.php';
+    ?>
+   <!-- Container -->
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+            <br>
+            <br>
+             
+            <h1 class="text-center">Controle Estoque</h1>
+            <?php
+            ?>
+            <hr>
+			</div>
+			<div class="col-lg-12 text-center">
+                     <div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">Widget</a> is loading comments...</div>
+                     <link rel="stylesheet" type="text/css" href="https://www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" />
+                     <script type="text/javascript" id="hcb"> /*<!--*/ if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"), h="https://www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&mod=%241%24wq1rdBcg%24bFgLH3kSaHER1iM.2tr6E1"+"&opts=16862&num=10&ts=1569521544251");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})(); /*-->*/ </script>
+			
+            <br>
+            <br>
+			
+			</div>
+			</div>
+			</div>
+			</div>
